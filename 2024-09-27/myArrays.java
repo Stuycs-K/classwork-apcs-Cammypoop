b3b53bf90fbf3b34baced7606e043e05991be78c
Steven Wu <steveph945@gmail.com>
2024-09-27 10:03:10 -0400

    Wrote aryToString

--- 2024-09-27/myArrays.java
+++ 2024-09-27/myArrays.java
@@ -1,7 +1,14 @@
 public class myArrays{
     public static String aryToString(int[] nums){
-        String out = "";
-        
+        String out="[";
+        for (int i=0;i<nums.length;i++){
+            out+=nums[i];
+            if (i!=nums.length-1){
+                out+="";
+            }
+        }
+        out+="]";
+        return out;
     }
     public static void main(String[] args) {
         int[] a = new int[]{1,3,4,5};

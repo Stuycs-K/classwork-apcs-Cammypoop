b947f5da868f7a486570608115b09d348e414607
Steven Wu <steveph945@gmail.com>
2024-09-27 09:54:11 -0400

    template

---
+++ 2024-09-27/myArrays.java
@@ -0,0 +1,10 @@
+public class myArrays{
+    public static String aryToString(int[] nums){
+
+    }
+    public static void main(String[] args) {
+        System.out.println(aryToString());
+        
+
+    }
+}

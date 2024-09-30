85060cbd4f13b8ba3f110235718a62a23d0a0e20
Steven Wu <steveph945@gmail.com>
2024-09-30 03:03:13 -0400

    writing returnCopy test cases

--- 2024-09-27/myArrays.java
+++ 2024-09-27/myArrays.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-
 public class myArrays{
     public static String aryToString(int[] nums){
         String out="[";
@@ -14,20 +12,27 @@ public class myArrays{
     }
     public static int[] returnCopy(int[]ary){
         int[] out = new int[ary.length];
-        for (int i =0;i < out.length - 1; i++){
+        for (int i = 0; i < out.length - 1; i++){
             out[i] = ary[i];
         }
         return out;
         
     }
     public static void main(String[] args) {
+        System.out.println("aryToCopy(int[] nums):");
         int[] a = {1,3,4,5};
-        System.out.println("Variable: " + "[1, 3, 4, 5]"  +". Returned: " + aryToString(a) + ".");
+        System.out.println("Variable: " + "[1, 3, 4, 5]"  + ". Returned: " + aryToString(a) + ".");
         a = new int[]{5,6,13};
-        System.out.println("Variable: " + "[5, 6, 13]"  +". Returned: " + aryToString(a) + ".");
+        System.out.println("Variable: " + "[5, 6, 13]"  + ". Returned: " + aryToString(a) + ".");
         a = new int[]{1};
-        System.out.println("Variable: " + "[1]"  +". Returned: " + aryToString(a) + ".");
-        
+        System.out.println("Variable: " + "[1]"  + ". Returned: " + aryToString(a) + ".");
 
+        System.out.println("returnCopy(int[] ary):");
+        a = new int[]{1,3,4,5};
+        System.out.println("Variable: " + "[1, 3, 4, 5]"  + ". Returned: " + returnCopy(a) + ".");
+        a = new int[]{5,6,13};
+        System.out.println("Variable: " + "[5, 6, 13]"  + ". Returned: " + returnCopy(a) + ".");
+        a = new int[]{1};
+        System.out.println("Variable: " + "[1]"  + ". Returned: " + returnCopy(a) + ".");
     }
 }

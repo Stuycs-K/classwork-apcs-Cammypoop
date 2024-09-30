11c2fc0f0a1635a11b4ca535df84ac044ec8d18a
Steven Wu <steveph945@gmail.com>
2024-09-30 03:10:54 -0400

    corrected test cases

--- 2024-09-27/myArrays.java
+++ 2024-09-27/myArrays.java
@@ -12,14 +12,14 @@ public class myArrays{
     }
     public static int[] returnCopy(int[]ary){
         int[] out = new int[ary.length];
-        for (int i = 0; i < out.length - 1; i++){
+        for (int i = 0; i < out.length; i++){
             out[i] = ary[i];
         }
         return out;
-        
     }
     public static void main(String[] args) {
         System.out.println("aryToCopy(int[] nums):");
+        // compare the strings
         int[] a = {1,3,4,5};
         System.out.println("Variable: " + "[1, 3, 4, 5]"  + ". Returned: " + aryToString(a) + ".");
         a = new int[]{5,6,13};
@@ -28,11 +28,13 @@ public class myArrays{
         System.out.println("Variable: " + "[1]"  + ". Returned: " + aryToString(a) + ".");
 
         System.out.println("returnCopy(int[] ary):");
+        System.out.println("(Correct output is false)");
+        // check if they have different addresses
         a = new int[]{1,3,4,5};
-        System.out.println("Variable: " + "[1, 3, 4, 5]"  + ". Returned: " + returnCopy(a) + ".");
+        System.out.println("Variable: " + "[1, 3, 4, 5]"  + ". Returned: " + (returnCopy(a) == a));
         a = new int[]{5,6,13};
-        System.out.println("Variable: " + "[5, 6, 13]"  + ". Returned: " + returnCopy(a) + ".");
+        System.out.println("Variable: " + "[5, 6, 13]"  + ". Returned: " + (returnCopy(a) == a));
         a = new int[]{1};
-        System.out.println("Variable: " + "[1]"  + ". Returned: " + returnCopy(a) + ".");
+        System.out.println("Variable: " + "[1]"  + ". Returned: " + (returnCopy(a) == a));
     }
 }

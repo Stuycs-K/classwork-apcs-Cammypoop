89ade6ebb24276f495163096918b2cb16deb7f14
Steven Wu <steveph945@gmail.com>
2024-10-01 11:25:18 -0400

    wrote the rest of test cases

--- 2024-09-30-2DArray/ArrayMethods.java
+++ 2024-09-30-2DArray/ArrayMethods.java
@@ -53,5 +53,44 @@ public class ArrayMethods {
         a = new int [][]{{-23, 1, 2},{9, 11},{}};
         System.out.println("Expected: 0" + "Output: " + arr2DSum(a));
 
+
+        int[] b = {2, 1, 3};
+        System.out.println("Expected: [2, 1, 3] " + "Output: " + arrToString(b));
+        b = new int[] {};
+        System.out.println("Expected: [] " + "Output: " + arrToString(b));
+        b = new int[] { 0, -5, 7, 10 };
+        System.out.println("Expected: [0, -5, 7, 10] " + "Output: " + arrToString(b));
+        b = new int[] { 9 };
+        System.out.println("Expected: [9] " + "Output: " + arrToString(b));
+
+
+        int[][] c = {{2, 1}, {3, 4, 5}, {1}};
+        System.out.println("Expected: [[2, 1], [3, 4, 5], [1]] " + "Output: " + arrToString(c));
+        c = new int[][] {{}, {}, {}};
+        System.out.println("Expected: [[], [], []] " + "Output: " + arrToString(c));
+        c = new int[][] {{23, 24, 29}, {}, {1}};
+        System.out.println("Expected: [[23, 24, 29], [], [1]] " + "Output: " + arrToString(c));
+        c = new int[][] {{1}, {2}, {3}};
+        System.out.println("Expected: [[1], [2], [3]] " + "Output: " + arrToString(c));
+        c = new int[][] {{-23, 1, 2}, {9, 11}, {}};
+        System.out.println("Expected: [[-23, 1, 2], [9, 11], []] " + "Output: " + arrToString(c));
+
+        
+        int[][] d = {{1, 2, 3}, {4, 5, 6}};
+        int[][] result = swapRC(d);
+        System.out.println("Expected: [[1, 4], [2, 5], [3, 6]] " + "Output: " + arrToString(result));
+        d = new int[][] {{7, 8}, {9, 10}, {11, 12}};
+        result = swapRC(d);
+        System.out.println("Expected: [[7, 9, 11], [8, 10, 12]] " + "Output: " + arrToString(result));
+        d = new int[][] {{1, 2}, {3, 4}};
+        result = swapRC(d);
+        System.out.println("Expected: [[1, 3], [2, 4]] " + "Output: " + arrToString(result));
+        d = new int[][] {{5}};
+        result = swapRC(d);
+        System.out.println("Expected: [[5]] " + "Output: " + arrToString(result));
+        d = new int[][] {{10, 20}, {30, 40}, {50, 60}, {70, 80}};
+        result = swapRC(d);
+        System.out.println("Expected: [[10, 30, 50, 70], [20, 40, 60, 80]] " + "Output: " + arrToString(result));
+
     }
 }

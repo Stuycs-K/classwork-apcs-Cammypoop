5820499341bcb15e7dbc17587cbf248dc8f3cd39
Steven Wu <steveph945@gmail.com>
2024-10-31 09:42:51 -0400

    sample code

---
+++ 2024-10-31-Advent/ReadFile.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class ReadFile {
+    public static void main(String[] args) {
+        try {
+            File file = new File("ReadFile.java");// 1
+            Scanner input = new Scanner(file);
+            // CODE THAT SCANS THE FILE.
+            input.close();// releases the file from your program
+
+        } catch (FileNotFoundException ex) {
+            // File not found what should you do?
+            System.out.println("File not found");
+            return; // you can return from a void function just don't put a value.
+        }
+    }
+}
--- 2024-10-31-Advent/Stuff.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class Stuff{
-    public static void main(String[] args){
-        Scanner sc = new Scanner("Test\n God has\nGiven");
-        while(sc.hasNext()){
-            System.out.println(sc.next());
-        }
-    }
-}

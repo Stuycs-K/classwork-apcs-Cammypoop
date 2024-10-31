91db1167b8981fc3e371485a8f0b3561e62f7a23
Steven Wu <steveph945@gmail.com>
2024-10-31 09:47:55 -0400

    its not printing

--- 2024-10-31-Advent/ReadFile.java
+++ 2024-10-31-Advent/ReadFile.java
@@ -1,11 +1,14 @@
-import java.util.Scanner;
+//1 you need all three imports.
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class ReadFile {
+
     public static void main(String[] args) {
+        // 2 Opening a file requires a try/catch
         try {
-            File file = new File("ReadFile.java");// 1
+            File file = new File("Test.java");// 1
             Scanner input = new Scanner(file);
             // CODE THAT SCANS THE FILE.
             input.close();// releases the file from your program

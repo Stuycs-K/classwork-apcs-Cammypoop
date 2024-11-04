252a2a2b0345979788654bff03e2e4e9ffcdc00c
Steven Wu <steveph945@gmail.com>
2024-11-04 03:53:26 -0500

    started part b

--- 2024-10-31-Advent/TriangleTester.java
+++ 2024-10-31-Advent/TriangleTester.java
@@ -29,7 +29,24 @@ public class TriangleTester {
 
     }
     public static int countTrianglesB(String filename) {
-
+        try {
+            int count = 0;
+            File file = new File(filename);
+            Scanner input = new Scanner(file);
+            while (input.hasNextLine()) {
+                int s1 = input.nextInt();
+                int s2 = input.nextInt();
+                int s3 = input.nextInt();
+                if (TestTriangle(s1, s2, s3)){
+                    count++;
+                }
+            }
+            input.close();
+            return count;
+        } catch (FileNotFoundException ex) {
+            System.out.println("File not found");
+            return 0;
+        }
     }
 
     public static void main(String[] args) {

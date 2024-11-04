c6d78802e2e7ab9305b2096dcd187e5fdbc26cee
Steven Wu <steveph945@gmail.com>
2024-11-04 03:44:51 -0500

    updated logic

--- 2024-10-31-Advent/TriangleTester.java
+++ 2024-10-31-Advent/TriangleTester.java
@@ -3,30 +3,34 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class TriangleTester {
-    public static boolean TestTriangle(int s1, int s2, int s3){
-        if (s1 + s2 > s3 || s2 + s3 > s1 || s1 + s3 > s2){
+    public static boolean TestTriangle(int s1, int s2, int s3) {
+        if (s1 + s2 > s3 || s2 + s3 > s1 || s1 + s3 > s2) {
             return true;
         }
         return false;
     }
-    public static int countTrianglesA(String filename){
+
+    public static int countTrianglesA(String filename) {
         int count = 0;
         try {
-            File file = new File("inputTri.txt");
+            File file = new File(filename);
             Scanner input = new Scanner(file);
-            while (input.hasNextLine()){
+            while (input.hasNextLine()) {
                 int s1 = input.nextInt();
                 int s2 = input.nextInt();
                 int s3 = input.nextInt();
                 TestTriangle(s1, s2, s3);
             }
             input.close();
-
+            return count;
         } catch (FileNotFoundException ex) {
             // File not found what should you do?
             System.out.println("File not found");
             return 0;
         }
-        return count;
+        
+    }
+    public static void main(String[] args) {
+        countTrianglesA("inputTri.txt");
     }
 }

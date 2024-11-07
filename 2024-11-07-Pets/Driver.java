bfb371d380c9390ebea9431039666e2abfa2dc98
Steven Wu <steveph945@gmail.com>
2024-11-07 09:55:59 -0500

    Tested Animal constructor, wrote Bird constructor

--- 2024-11-07-Pets/Driver.java
+++ 2024-11-07-Pets/Driver.java
@@ -1,5 +1,11 @@
-public class Driver{
-  public static void main(String[] args){
-    
+public class Driver
+{
+  public static void main(String[] args)
+  {
+    Animal animal1 = new Animal("moo", 13, "Cow");
+    animal1.speak();
+    Animal animal2 = new Animal("woof", 5, "Dog");
+    animal2.speak();
   }
 }
+ 

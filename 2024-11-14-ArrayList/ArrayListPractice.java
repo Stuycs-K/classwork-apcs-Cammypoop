527782cf3b4fb20149d9f3cd3ccc9f61f265df47
Steven Wu <steveph945@gmail.com>
2024-11-14 09:54:15 -0500

    fixed createRandomArray() to actually work

--- 2024-11-14-ArrayList/ArrayListPractice.java
+++ 2024-11-14-ArrayList/ArrayListPractice.java
@@ -1,12 +1,14 @@
+import java.util.*;
 public class ArrayListPractice{
   public static void main(String[] args) {
-    System.out.println()
+    System.out.println(createRandomArray(5));
+    System.out.println(createRandomArray(15));
   }
   public static ArrayList<String>createRandomArray(int size){
     ArrayList<String> out = new ArrayList<String>(size);
     for (int i = 0; i < size; i++){
-      ArrayList.add((int)(Math.random() * 11) + "");
-      if out.get(i).equals("0"){
+      out.add("" + (int)(Math.random() * 11));
+      if (out.get(i).equals("0")){
         out.set(i, "");
       }
     }

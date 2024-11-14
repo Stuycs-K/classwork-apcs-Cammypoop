8d5a54aa492e7fe0542f49d0dd3c72cc95b9894a
Steven Wu <steveph945@gmail.com>
2024-11-14 10:01:22 -0500

    wrote and tested replaceEmpty

--- 2024-11-14-ArrayList/ArrayListPractice.java
+++ 2024-11-14-ArrayList/ArrayListPractice.java
@@ -1,8 +1,10 @@
 import java.util.*;
 public class ArrayListPractice{
   public static void main(String[] args) {
-    System.out.println(createRandomArray(5));
-    System.out.println(createRandomArray(15));
+    ArrayList<String> test = createRandomArray(30);
+    System.out.println(test);
+    replaceEmpty(test);
+    System.out.println(test);
   }
   public static ArrayList<String>createRandomArray(int size){
     ArrayList<String> out = new ArrayList<String>(size);
@@ -14,4 +16,23 @@ public class ArrayListPractice{
     }
     return out;
   }
+
+
+public static void replaceEmpty( ArrayList<String> original){
+  //Modify the ArrayList such that it has all of the empty strings are
+  //replaced with the word "Empty".
+while (original.indexOf("") >= 0){
+  original.set(original.indexOf(""), "Empty");
+}
+}
+
+//public static ArrayList<String> makeReversedList( ArrayList<String> original){
+  //return a new ArrayList that is in the reversed order of the original.
+//}
+
+//public static ArrayList<String> mixLists( ArrayList<String> a,  ArrayList<String> b){
+  //return a new ArrayList that has all values of a and b in alternating order that is:
+  //a[0], b[0], a[1], b[1]...
+  //If one list is longer than the other, just attach the remaining values to the end.
+//}
 }

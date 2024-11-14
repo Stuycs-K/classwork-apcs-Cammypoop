d1845345653f8631ff17fb30443185242f0be39c
Steven Wu <steveph945@gmail.com>
2024-11-14 10:13:42 -0500

    wrote mixLists()

--- 2024-11-14-ArrayList/ArrayListPractice.java
+++ 2024-11-14-ArrayList/ArrayListPractice.java
@@ -1,11 +1,13 @@
 import java.util.*;
 public class ArrayListPractice{
   public static void main(String[] args) {
-    ArrayList<String> test = createRandomArray(100);
+    ArrayList<String> test = createRandomArray(10);
     System.out.println(test);
     replaceEmpty(test);
     System.out.println(test);
     System.out.println(makeReversedList(test));
+
+    System.out.println(mixLists(test,test));
   }
   public static ArrayList<String>createRandomArray(int size){
     ArrayList<String> out = new ArrayList<String>(size);
@@ -37,9 +39,26 @@ public static ArrayList<String> makeReversedList( ArrayList<String> original){
   return out;
 }
 
-//public static ArrayList<String> mixLists( ArrayList<String> a,  ArrayList<String> b){
+public static ArrayList<String> mixLists( ArrayList<String> a,  ArrayList<String> b){
   //return a new ArrayList that has all values of a and b in alternating order that is:
   //a[0], b[0], a[1], b[1]...
   //If one list is longer than the other, just attach the remaining values to the end.
-//}
+    ArrayList<String> out = new ArrayList<String>(a.size() + b.size());
+    int minLen = Math.min(a.size(), b.size());
+    for (int i = 0; i < minLen; i++){
+      out.add(a.get(i));
+      out.add(b.get(i));
+    }
+  if (a.size() >= minLen){
+    for (int i = minLen; i < a.size(); i++){
+      out.add(a.get(i));
+    }
+  }
+  else{
+    for (int i = minLen; i < b.size(); i++){
+      out.add(b.get(i));
+    }
+  }
+  return out;
+}
 }

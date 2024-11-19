2c5e56e283bb2ff8168f5c115f2b94fdad12497b
Steven Wu <steveph945@gmail.com>
2024-11-19 10:03:03 -0500

    writing scanner

---
+++ 2024-11-18-advent2016/Day2.java
@@ -0,0 +1,52 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class Day2 {
+
+    public static int countDistA(String filename) {
+        try {
+            int direct = 0;
+            int xDist = 0;
+            int yDist = 0;
+            File file = new File(filename);
+            Scanner input = new Scanner(file);
+            while (input.hasNext()) {
+              String d = input.next();
+              int blocks = Integer.parseInt(d.substring(1,d.length() - 1));
+              if (d.charAt(0) == 'L'){
+                direct += 3;
+                direct %= 4;
+              }
+              if (d.charAt(0) == 'R'){
+                direct++;
+                direct %= 4;
+              }
+              if (direct == 0){
+                yDist += blocks;
+              }
+              if (direct == 1){
+                xDist += blocks;
+              }
+              if (direct == 2){
+                yDist -= blocks;
+              }
+              if (direct == 3){
+                xDist -= blocks;
+              }
+            }
+            input.close();
+            return (Math.abs(xDist) + Math.abs(yDist));
+        } catch (FileNotFoundException ex) {
+            System.out.println("File not found");
+            return 0;
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+      System.out.println(countDistA("Day2In.txt"));
+    }
+}

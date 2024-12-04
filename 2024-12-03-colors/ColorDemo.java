e9fa3c17dbcb911cec0559a79b0597c3f9b7961e
Steven Wu <steveph945@gmail.com>
2024-12-04 04:29:12 -0500

    main

--- 2024-12-03-colors/ColorDemo.java
+++ 2024-12-03-colors/ColorDemo.java
@@ -6,28 +6,40 @@ public class ColorDemo {
     public static final String MOVE_CURSOR_HOME = "\u001b[H";
 
     public static void color(int foreground, int background) {
-        System.out.print("\u001b[" + foreground + ";" + (10 + background) + "m");
+        System.out.print("\u001b[" + foreground + ";" + background + "m");
     }
 
     public static void color(int foreground, int background, int modifier) {
-        System.out.print("\u001b[" + foreground + ";" + (10 + background) + ";" + modifier + "m");
+        System.out.print("\u001b[" + foreground + ";" + background + ";" + modifier + "m");
     }
+
     public static void drawGrid(int cycle) {
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
-                int foreground = 31 + (cycle + i + j) % 7;  
-                int background = 41 + (cycle + i + j) % 7;  
+                int foreground = 31 + (cycle + i + j) % 6;
+                int background = 41 + (cycle + i + j) % 6;
                 color(foreground, background);
-                System.out.print("   ");  //
+                System.out.print("  ");
             }
-            System.out.println(RESET);  // 
+            System.out.println(RESET);
         }
     }
+
+    public static void sleep(int milli) {
+        try {
+            Thread.sleep(milli);
+        } catch (Exception e) {
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.print(RESET + CLEAR_SCREEN + MOVE_CURSOR_HOME);
-        color(36, 35, 4);
-        System.out.println("Hello, colors!");
-        System.out.print(SHOW_CURSOR + RESET);
-        
+        System.out.print(CLEAR_SCREEN + HIDE_CURSOR);
+        for (int cycle = 0; cycle < 20; cycle++) {
+            System.out.print(MOVE_CURSOR_HOME);
+            drawGrid(cycle);
+            sleep(200);
+        }
+        System.out.print(RESET + SHOW_CURSOR);
+
     }
 }

cb94d1cc2c2c15a5e33345d9442eb19608975d66
Steven Wu <steveph945@gmail.com>
2024-12-09 15:24:11 -0500

    loop works

--- 2024-12-06-Adventurer/Driver.java
+++ 2024-12-06-Adventurer/Driver.java
@@ -1,27 +1,51 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class Driver {
     public static String heroAction(Adventurer hero, Adventurer enemy, String input) {
         switch (input) {
-        case "q":
-        case "quit":
-            System.exit(0);
-            return "";
-        case "a":
-        case "attack":
-            return hero.attack(enemy);
-        case "sp":
-        case "special":
-            return hero.specialAttack(enemy);
-        case "su":
-        case "support":
-            return hero.support();
-        default:
-            return "Invalid Input, enter again";
+            case "q":
+            case "quit":
+                System.exit(0);
+                return ""; // satisfy compiler
+            case "a":
+            case "attack":
+                return hero.attack(enemy);
+            case "sp":
+            case "special":
+                return hero.specialAttack(enemy);
+            case "su":
+            case "support":
+                return hero.support();
+            default:
+                return "Invalid Input, enter again";
         }
     }
 
-    
+    public static String enemyRNGAction(Adventurer hero, Adventurer enemy) {
+        Random rand = new Random();
+        int input = rand.nextInt(3);
+        switch (input) {
+            case 0:
+                return enemy.attack(hero);
+            case 1:
+                return enemy.specialAttack(hero);
+            case 2:
+                return enemy.support();
+            default:
+                return ""; // satisfy compiler
+        }
+    }
+
+    public static String deathCheck(Adventurer hero, Adventurer enemy) {
+        if (hero.getHP() <= 0) {
+            return "You died.";
+        } else if (enemy.getHP() <= 0) {
+            return enemy.getName() + " died.";
+        } else {
+            return "";
+        }
+    }
 
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
@@ -30,10 +54,14 @@ public class Driver {
         Adventurer hero;
 
         System.out.println("Enter your username: ");
-        String userName = userInput.nextLine();
-        hero = new Bard(userName);
-        System.out.println("Your username is: " + userName);
-        System.out.println("Fight!");
+        String user = userInput.nextLine();
+        hero = new Bard(user);
+        System.out.println("Your username is: " + user);
+        System.out.println("Enter your enemy's username: ");
+        String enemyUser = userInput.nextLine();
+        hero = new Bard(enemyUser);
+        System.out.println("Your enemy's username is: " + enemyUser);
+        System.out.println("\nFighting " + enemy.getName() + "!");
         while (true) {
             System.out.println(
                     hero.getName() + ", " + hero.getHP() + "/" + hero.getmaxHP() + " HP, " + hero.getSpecial() + "/"
@@ -46,9 +74,18 @@ public class Driver {
 
             String returned = heroAction(hero, enemy, input);
             System.out.println(returned);
-
             if (returned.equals("Invalid Input, enter again")) {
                 continue;
+            }
+            if (deathCheck(hero, enemy) != "") {
+                System.out.println(deathCheck(hero, enemy));
+                System.exit(0);
+            }
+            System.out.println(enemyRNGAction(hero, enemy));
+
+            if (deathCheck(hero, enemy) != "") {
+                System.out.println(deathCheck(hero, enemy));
+                System.exit(0);
             }
         }
 

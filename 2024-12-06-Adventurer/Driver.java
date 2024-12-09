9d3c0ba7605d8339abd470015714da4d98f0706f
Steven Wu <steveph945@gmail.com>
2024-12-09 06:44:19 -0500

    testing

--- 2024-12-06-Adventurer/Driver.java
+++ 2024-12-06-Adventurer/Driver.java
@@ -2,12 +2,12 @@ public class Driver{
 
   public static void main(String[] args){
     Adventurer enemy = new Bard("Target Dummy", 100);
-
     Adventurer hero = new Bard("Chad", 100);
-
     Adventurer teammate = new Bard("Brad", 100);
 
     System.out.println(hero.attack(enemy));
     System.out.println(hero.support(teammate));
+    System.out.println(hero.support());
+    System.out.println(hero.specialAttack(enemy));
   }
 }

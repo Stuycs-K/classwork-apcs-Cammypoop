d9f82906aa0ca3fdd8b737fa270936b110f0d529
Steven Wu <steveph945@gmail.com>
2025-01-02 10:00:51 -0500

    fib under 10s

---
+++ 2025-01-02-Fibonacci/Fib.java
@@ -0,0 +1,11 @@
+public class Fib{
+  public static int fib(int n){
+    if (n<2){
+      return n;
+    }
+    return fib(n-2) + fib(n-1);
+  }
+  public static void main(String[] args){
+    fib(Integer.parseInt(args[0]));
+  }
+}

5155ed9a9a9bcf868152247c0634a7436eeb2fe9
Steven Wu <steveph945@gmail.com>
2025-01-06 09:29:25 -0500

    save

---
+++ 2025-01-02-Fibonacci/ToWords.java
@@ -0,0 +1,56 @@
+public class ToWords{
+  public static String toWords(int n){
+    if (n <= 13 || n == 15){
+      switch (n){
+        case 0:
+          return "zero";
+          break;
+        case 1:
+          return "one";
+          break;
+        case 2:
+          return "two"
+          break;
+        case 3;
+          return ""
+          break;
+        case 4:
+
+          break;
+        case 5:
+
+          break;
+        case 6:
+
+          break;
+        case 7:
+
+          break;
+        case 8:
+
+          break;
+        case 9:
+
+          break;
+        case 10:
+
+          break;
+        case 11:
+
+          break;
+        case 12:
+
+          break;
+        case 13:
+
+          break;
+        case 15:
+
+          break;
+      }
+    }
+  }
+  public static void main(String[] args) {
+
+  }
+}
